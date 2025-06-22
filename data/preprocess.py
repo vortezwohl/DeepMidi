a7137324f64628bee11b94b41f1aa11ef1cdaf68
吴子豪 <vortezwohl@proton.me>
2025-06-22 16:53:09 +0800

    feat: 添加数据集

--- data/preprocess.py
+++ data/preprocess.py
@@ -3,6 +3,9 @@ import random
 import time
 
 import requests
+from music21 import converter, note, chord
+from deeplotx.util import get_files
+from vortezwohl.concurrent import ThreadPool
 
 
 def download(url: str, filename: str, interval: int = 0) -> str:
@@ -10,7 +13,7 @@ def download(url: str, filename: str, interval: int = 0) -> str:
         return f'File "{filename}" already exists.'
     try:
         if interval > 0:
-            time.sleep(random.randint(0, interval))
+            time.sleep(random.randint(int(interval / 2 // 1), interval))
         resp = requests.get(url, stream=True)
         resp.raise_for_status()
         with open(filename, 'wb') as f:
@@ -19,3 +22,37 @@ def download(url: str, filename: str, interval: int = 0) -> str:
         return f'File from "{url}" is downloaded to "{filename}".'
     except requests.exceptions.RequestException as e:
         return f'File failed to download: {e}.'
+
+
+def analyse_midi(filename: str, freq: bool = False, mode: str = 'note') -> tuple[str, list | None]:
+    def convert_name(_note: str):
+        return _note.replace('-flat', 'b').replace('-sharp', '#').replace('in octave', '_').replace(' ', '')
+    res = []
+    try:
+        s = converter.parse(filename)
+        for e in s.flatten():
+            match mode:
+                case 'note':
+                    if isinstance(e, note.Note):
+                        if freq:
+                            res.append((e.pitch.frequency, e.duration.quarterLength))
+                        else:
+                            res.append((convert_name(e.pitch.fullName), e.duration.quarterLength))
+                case 'chord':
+                    if isinstance(e, chord.Chord):
+                        if freq:
+                            pitches = '|'.join(str(p.frequency) for p in e.pitches)
+                        else:
+                            pitches = '|'.join(convert_name(p.fullName) for p in e.pitches)
+                        res.append((pitches, e.duration.quarterLength))
+        return filename, res
+    except:
+        return filename, None
+
+
+def analyse_all_midi(path: str, freq: bool = False, mode: str = 'note') -> list:
+    workers = ThreadPool(64)
+    files = get_files(path)
+    inputs = zip(files, [freq] * len(files), [mode] * len(files))
+    results = workers.gather([analyse_midi] * len(inputs), inputs)
+    return [(filename, midi) for filename, midi in results]
